bf0d0efc71d11efa97feb17c3d2a6b22a7225310
kramer00 <jeffkramer00@gmail.com>
2014-10-31 10:57:29 -0700

    first commit

---
+++ config/constants.php
@@ -0,0 +1,26 @@
+<?php
+	//this is a PHP comment
+	//begin - constants for database connection
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "root");
+	//end - constants for database connection
+	
+	//begin - constants for databases and tables
+	define("DB_NAME", "sweetcorner");
+	
+	define("TBL_PAGES", "`pages`");
+	define("TBL_SCHEDULE", "`schedule`");
+	//end - constants for databases and tables
+	
+	//begin - constants for project directories
+	define("DIR_JS", "js");
+	define("DIR_CSS", "css");
+	define("DIR_IMG", "img");
+	//end - constants for project directories
+	
+	//begin - email constants
+	define("MAIL_FROM", "enter-your@server.email");
+	define("MAIL_TO", "enter-your@contact.email");
+	//end - email constants
+?>
---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+	//we connect to the MySQL database using the host name, the user and the password:
+	$db_link = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+	if (!$db_link) { //in case of error, mysql_connect() returns FALSE; !$something ($something negated) means that $something is not TRUE
+		die("Could not connect to the database: ".mysql_error()); //mysql_error() returns the error message from the previous operation
+	}
+	
+	//then, we select the database:
+	$db_selected = mysql_select_db(DB_NAME, $db_link); //similarly to mysql_connect(), mysql_select_db() returns FALSE in case of error
+	if (!$db_selected) {
+		die ("Cannot use database: ".mysql_error());
+	}
+?>
